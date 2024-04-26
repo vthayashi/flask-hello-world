aed7e0ff89a5bfae35a1e433bda4a2968a551045
Victor Hayashi <vtakashi@hotmail.com>
2024-04-26 19:24:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Victor!'
+@app.route("/")
+def hello():
+    return "Olá Anônimo!"
+
+@app.route("/user/config")
+def userconfig():
+    return "Olá Usuário!"
+
+@app.route("/admin/config")
+def adminconfig():
+    return "Olá Administrador!"
